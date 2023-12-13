461e5d3af12d19dce79c86550b19ac25f8d07cb7
Khalilkma <khalilkma757.khalid@gmail.com>
2023-12-13 14:52:51 -0300

    Board Exception

--- src/App/programa.java
+++ src/App/programa.java
@@ -7,7 +7,6 @@ import JogoTabuleiro.Tabuleiro;
 public class programa {
     public static void main(String[] args) {
         PartidaXadrez partidaXadrez = new PartidaXadrez();
-
         UI.printTabuleiro(partidaXadrez.getPieces());
     }
 }
--- src/JogoTabuleiro/Tabuleiro.java
+++ src/JogoTabuleiro/Tabuleiro.java
@@ -7,6 +7,9 @@ public class Tabuleiro {
     private Piece[][] pieces;
 
     public Tabuleiro(int linhas, int colunas) {
+        if(linhas < 1 || colunas < 1){
+            throw new TabuleiroException("Erro criando tabuleiro: é necessário que haja pelo menos uma linha e uma coluna");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         pieces = new Piece[linhas][colunas];
@@ -16,28 +19,46 @@ public class Tabuleiro {
         return linhas;
     }
 
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
-    }
 
     public int getColunas() {
         return colunas;
     }
 
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
 
-    public Piece piece(int linhas, int colunas){
-        return pieces[linhas][colunas];
+    public Piece piece(int linha, int coluna){
+        if(!positionExistis(linha, coluna)){
+            throw new TabuleiroException("Posição fora do tabuleiro");
+        }
+        return pieces[linha][coluna];
     }
 
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new TabuleiroException("Posição fora do tabuleiro");
+        }
         return pieces[position.getLinha()][position.getColuna()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new TabuleiroException("Já tem uma peça na posição" + position);
+        }
         pieces[position.getLinha()][position.getColuna()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExistis(int linha, int coluna){
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExistis(position.getLinha(), position.getColuna());
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new TabuleiroException("Posição fora do tabuleiro");
+        }
+        return piece(position) != null;
     }
 }
